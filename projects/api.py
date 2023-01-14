611288f342b1012b902519319880818b9aeebf6a
Josue <sjosue5082002@gmail.com>
2023-01-14 17:08:51 -0600

    Majora en los permisos

--- projects/api.py
+++ projects/api.py
@@ -1,11 +1,12 @@
 from .models import Project
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .serializers import ProjectSerializer
+from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 
 class ProjectViewSet(viewsets.ModelViewSet):
 
     queryset = Project.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        IsAuthenticated
     ]
     serializer_class = ProjectSerializer
