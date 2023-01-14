565eba1ce0a6e7872ef09a7a1fefd9227c8dc99c
Josue <sjosue5082002@gmail.com>
2023-01-13 21:03:59 -0600

    Listo para desplegar en Render 3

--- apirestcrud/wsgi.py
+++ apirestcrud/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+import sys
+sys.path.append('/home/django_projects/MyProject')
+sys.path.append('/home/django_projects/MyProject/project_name')
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apirestcrud.settings')
